0d85aa2d572d15e1515ba82802aaeca7d19941c3
essaji <essaji44@gmail.com>
2016-07-16 23:06:48 +0500

    Second Commit

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,7 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
 import {Component} from '@angular/core';
 
 @Component({
     selector: 'app',
     template: 'Hello Angular 2'
 })
-class AppCmp{}
-
-bootstrap(AppCmp);
+export class AppCmp{}
